09b176cef6ba60a16ff5113bd2e8dddd492513a8
Debjit Chattopadhyay <debjitchatterjee31@gmail.com>
2025-03-18 13:47:21 +0530

    adding https changes

--- src/components/FeeList.js
+++ src/components/FeeList.js
@@ -11,7 +11,7 @@ function FeeList() {
 
   const fetchFees = async () => {
     try {
-      const response = await fetch('http://3.111.84.98:61001/api/v1/getFeeList');
+      const response = await fetch('https://43.204.98.7:443/api/v1/getFeeList');
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
@@ -30,7 +30,7 @@ function FeeList() {
 
   const handleDeleteClick = async (id) => {
     try {
-      const response = await fetch(`http://3.111.84.98:61001/api/v1/deleteTransaction/${id}`, {
+      const response = await fetch(`https://43.204.98.7:443/api/v1/deleteTransaction/${id}`, {
         method: 'DELETE',
       });
 
